12b8378469e1653eb27a6e2589dffd9f77418abf
Aashika2002 <aashika.2024csit1139@kiet.edu>
2024-01-07 12:19:11 +0530

    Changed faq

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
     const faq = [
         {
-            question: "What is Netflix?",
+            question: "What is  a Netflix?",
             answer: "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.",
         },
         {
